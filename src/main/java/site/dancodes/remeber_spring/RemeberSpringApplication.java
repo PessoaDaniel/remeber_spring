5d1aff5c04e23fd8244aea28cf98661dcdfe4c98
Dancodes <danielpessoa432@gmail.com>
2024-05-03 11:18:40 -0300

    initial commit

---
+++ src/main/java/site/dancodes/remeber_spring/RemeberSpringApplication.java
@@ -0,0 +1,13 @@
+package site.dancodes.remeber_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemeberSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemeberSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/dancodes/remeber_spring/RemeberSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dancodes.remeber_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemeberSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
